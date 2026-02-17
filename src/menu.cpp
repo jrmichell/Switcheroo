a13bb31bf2eed4fd5c720e81045b8e15a52c98a5
Jack Michell <jmichell2003@gmail.com>
2026-02-17 10:27:20 -0600

    feat(menu): add options to display records and remove duplicates

--- src/menu.cpp
+++ src/menu.cpp
@@ -7,17 +7,8 @@ FileType Menu::read_input_file_type() {
     std::print("Enter the relative file path: ");
     std::cin >> m_converter.get_input_file_path();
 
-    FileType file_type = m_converter.read_file_ext(m_converter.get_input_file_path());
-
-    switch (file_type) {
-        case FileType::CSV:
-        case FileType::JSON:
-            m_converter.convert();
-            break;
-        case FileType::NONE:
-            std::println("Failed to read file extension.");
-            break;
-    }
+    fs::path input_path = m_converter.get_input_file_path();
+    FileType file_type = m_converter.read_file_ext(input_path);
 
     return file_type;
 }
@@ -28,7 +19,9 @@ void Menu::prompt_menu() {
     std::println("--- Switcheroo ---");
     do {
         std::println("\n[1] Convert");
-        std::println("[2] Quit\n");
+        std::println("[2] Display File Content");
+        std::println("[3] Remove Duplicates");
+        std::println("[4] Quit\n");
         std::print("Enter an option: ");
 
         if (!(std::cin >> option)) {
@@ -42,15 +35,37 @@ void Menu::prompt_menu() {
         switch (option) {
         case 1:
             file_type = read_input_file_type();
-            if (file_type != FileType::NONE) {
-                break;
+            switch (file_type) {
+                case FileType::CSV:
+                case FileType::JSON:
+                    m_converter.convert();
+                    break;
+                case FileType::NONE:
+                    std::println("Failed to read file extension.");
+                    break;
             }
             break;
         case 2:
+            file_type = read_input_file_type();
+            if (file_type == FileType::NONE) {
+                std::println("Failed to read file extension.");
+                continue;
+            }
+            m_converter.csv_display_contents(m_converter.get_input_file_path()); 
+            break;
+        case 3:
+            file_type = read_input_file_type();
+            if (file_type != FileType::CSV) {
+                std::println("You can only remove duplicates from a CSV.");
+                continue;
+            }
+            m_converter.csv_remove_duplicate_records();
+            break;
+        case 4:
             std::println("\nGoodbye!");
             break;
         default:
             std::println("\nPlease enter a valid option.");
         }
-    } while (option != 2);
+    } while (option != 4);
 }
--- src/menu.hpp
+++ src/menu.hpp
@@ -1,3 +1,5 @@
+#pragma once
+
 #include "converter.hpp"
 
 class Menu {
